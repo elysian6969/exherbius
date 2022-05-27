a3cb263fb9a31c0b5f37db19719b8bab7464595a
elysian <tsujianii@tuta.io>
2022-05-28 03:18:26 +0800

    morb

---
+++ readme.md
@@ -0,0 +1,149 @@
+download [exherbo musl tarball](https://dev.exherbo.org/stages/exherbo-x86_64-pc-linux-musl-current.tar.xz)
+
+you know what to do
+
+flag key
+
+`-1`/`--preserve-world`
+
+`-x`/`--execute`
+
+`-0`/`--no-dependencies-from`
+
+`-m`/`--make`
+
+`-4`/`--cross-host`
+
+argument to `-m`/`--make` key
+
+`x`/`cross-compile`
+
+configuration
+
+`/etc/paludis/bashrc`
+```
+CHOST="x86_64-pc-linux-musl"
+
+i686_pc_linux_gnu_CFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+i686_pc_linux_gnu_CXXFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+
+x86_64_pc_linux_gnu_CFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+x86_64_pc_linux_gnu_CXXFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+
+x86_64_pc_linux_musl_CFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+x86_64_pc_linux_musl_CXXFLAGS="-march=native -Ofast -fomit-frame-pointer -pipe"
+```
+
+`/etc/paludis/options.conf`
+
+```
+*/* build_options: jobs=12 -recommended_tests
+*/* targets: i686-pc-linux-gnu x86_64-pc-linux-gnu x86_64-pc-linux-musl
+*/* lua_abis: 5.2
+
+*/* -bash-completion -python
+
+sys-apps/paludis scm
+sys-devel/gcc -threads
+```
+
+`/etc/palduis/repositories/i686-pc-linux-gnu.conf`
+
+```
+format = exndbam
+location = ${root}/var/db/paludis/repositories/cross-installed/i686-pc-linux-gnu
+name = i686-pc-linux-gnu
+split_debug_location = /usr/i686-pc-linux-gnu/lib/debug
+tool_prefix = i686-pc-linux-gnu-
+cross_compile_host = i686-pc-linux-gnu
+```
+
+`/etc/paludis/repositories/x86_64-pc-linux-gnu.conf`
+
+```
+format = exndbam
+location = ${root}/var/db/paludis/repositories/cross-installed/x86_64-pc-linux-gnu
+name = x86_64-pc-linux-gnu
+split_debug_location = /usr/x86_64-pc-linux-gnu/lib/debug
+tool_prefix = x86_64-pc-linux-gnu-
+cross_compile_host = x86_64-pc-linux-gnu
+```
+
+`sys-kernel/linux-headers`
+
+```bash
+cave resolve sys-kernel/linux-headers -x1
+cave resolve -mx -4 i686-pc-linux-gnu sys-kernel/linux-headers -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu sys-kernel/linux-headers -x1
+```
+
+`sys-libs/glibc`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu -0 sys-libs/glibc sys-libs/glibc -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu -0 sys-libs/glibc sys-libs/glibc -x1
+```
+
+fix missing compiler shit, lmao!
+
+```
+cd /usr/host/bin
+
+# c compiler
+ln -s i686-pc-linux-gnu-gcc-11 i686-pc-linux-gnu-cc
+ln -s x86_64-pc-linux-gnu-gcc-11 x86_64-pc-linux-gnu-cc
+
+# c preprocessor
+ln -s cpp i686-pc-linux-gnu-cpp
+ln -s cpp x86_64-pc-linux-gnu-cpp
+
+# c++ compiler
+ln -s i686-pc-linux-gnu-g++-11 i686-pc-linux-gnu-c++
+ln -s x86_64-pc-linux-gnu-g++-11 x86_64-pc-linux-gnu-c++
+```
+
+`sys-libs/glibc` (again)
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu sys-libs/glibc -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu sys-libs/glibc -x1
+```
+
+`sys-libs/libstdc++`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu sys-libs/libstdc++ -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu sys-libs/libstdc++ -x1
+```
+
+`dev-libs/gmp`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu -0 dev-libs/gmp dev-libs/gmp -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu -0 dev-libs/gmp dev-libs/gmp -x1
+```
+
+`dev-libs/mpfr`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu -0 dev-libs/mpfr dev-libs/mpfr -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu -0 dev-libs/mpfr dev-libs/mpfr -x1
+```
+
+`dev-libs/mpc`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu -0 dev-libs/mpc dev-libs/mpc -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu -0 dev-libs/mpc dev-libs/mpc -x1
+```
+
+`dev-libs/zlib`
+
+```bash
+cave resolve -mx -4 i686-pc-linux-gnu -0 dev-libs/zlib dev-libs/zlib -x1
+cave resolve -mx -4 x86_64-pc-linux-gnu -0 dev-libs/zlib dev-libs/zlib -x1
+```
+
+### thanks
+
+- [exherbo docs/multiarch](https://www.exherbo.org/docs/multiarch/multiarch.html)
---
+++ src/main.rs
@@ -0,0 +1,176 @@
+use clap::Parser;
+use mix_triple::{Arch, Env, Triple};
+use std::collections::BTreeSet;
+use std::ffi::OsStr;
+use std::fs;
+use std::process::Command;
+
+const I686_DB: &str = "/var/db/paludis/repositories/cross-installed/i686-pc-linux-gnu/data";
+const X86_64_DB: &str = "/var/db/paludis/repositories/cross-installed/x86_64-pc-linux-gnu/data";
+const DB: &str = "/var/db/paludis/repositories/installed/data";
+
+fn read_db(db: &str) -> BTreeSet<String> {
+    let mut packages = BTreeSet::new();
+
+    for entry in fs::read_dir(db).expect("read database") {
+        let entry = entry.expect("read database entry");
+        let path = entry.path();
+        let name = path.file_name().expect("file name");
+        let name = name.to_str().expect("file name");
+        let name = name.replace("---", "/");
+
+        packages.insert(name);
+    }
+
+    packages
+}
+
+fn cave() -> Command {
+    let mut command = Command::new("cave");
+
+    command
+        .current_dir("/var/tmp/paludis")
+        .env_clear()
+        .env("HOME", "/var/tmp/paludis")
+        .env("PALUDIS_DO_NOTHING_SANDBOXY", "1")
+        .env("LANG", "en_US.UTF-8")
+        .env("LC_ALL", "en_US.UTF-8")
+        .env("PATH", "/milk/global:/usr/x86_64-pc-linux-musl/bin")
+        .env("TERM", "xterm-256color");
+
+    command
+}
+
+fn resolve_main<I, S>(packages: I)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut command = cave();
+
+    command
+        .arg("resolve")
+        .args(["--no-blockers-from", "*/*"])
+        .arg("--preserve-world")
+        .args(packages)
+        .arg("--execute");
+
+    println!("{command:?}");
+
+    let mut child = command.spawn().expect("cave resolve");
+
+    let _wait = child.wait();
+}
+
+fn resolve_i686<I, S>(packages: I)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut command = cave();
+
+    command
+        .args(["--environment", ":gnu"])
+        .arg("resolve")
+        .args(["--cross-host", "i686-pc-linux-gnu"])
+        .args(["--make", "cross-compile"])
+        .arg("--preserve-world")
+        .args(packages)
+        .arg("--execute");
+
+    println!("{command:?}");
+
+    let mut child = command.spawn().expect("cave resolve");
+
+    let _wait = child.wait();
+}
+
+fn resolve_x86_64<I, S>(packages: I)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut command = cave();
+
+    command
+        .args(["--environment", ":gnu"])
+        .arg("resolve")
+        .args(["--cross-host", "x86_64-pc-linux-gnu"])
+        .args(["--make", "cross-compile"])
+        .arg("--preserve-world")
+        .args(packages)
+        .arg("--execute");
+
+    println!("{command:?}");
+
+    let mut child = command.spawn().expect("cave resolve");
+
+    let _wait = child.wait();
+}
+
+#[derive(Parser)]
+struct Add {
+    #[clap(default_value = Triple::host().as_str(), short = 't', long)]
+    triple: Triple,
+    list: Vec<String>,
+}
+
+#[derive(Parser)]
+struct Remove {
+    #[clap(default_value = Triple::host().as_str(), short = 't', long)]
+    triple: Triple,
+    list: Vec<String>,
+}
+
+#[derive(Parser)]
+enum Options {
+    #[clap(alias = "a")]
+    Add(Add),
+
+    #[clap(alias = "r")]
+    Remove(Remove),
+
+    #[clap(alias = "s")]
+    Stat,
+}
+
+fn resolve<I, S>(packages: I, triple: Triple)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let resolve = match triple.as_tuple() {
+        (Arch::i686, _sys, Env::Gnu) => resolve_i686,
+        (Arch::x86_64, _sys, Env::Gnu) => resolve_x86_64,
+        (Arch::x86_64, _sys, Env::Musl) => resolve_main,
+        _ => panic!("not supported"),
+    };
+
+    resolve(packages)
+}
+
+fn main() {
+    let options = Options::parse();
+
+    match options {
+        Options::Add(add) => resolve(add.list, add.triple),
+        Options::Remove(remove) => resolve(
+            remove.list.into_iter().map(|pkg| format!("!{pkg}")),
+            remove.triple,
+        ),
+        Options::Stat => {
+            let main = read_db(DB);
+            let i686 = read_db(I686_DB);
+            let x86_64 = read_db(X86_64_DB);
+
+            let x86_64_musl = main.len();
+            let i686_gnu = i686.len();
+            let x86_64_gnu = x86_64.len();
+            let total = x86_64_musl + i686_gnu + x86_64_gnu;
+
+            println!(
+                "{total} (x86_64-musl {x86_64_musl}, i686-gnu {i686_gnu}, x86_64-gnu {x86_64_gnu})"
+            );
+        }
+    }
+}
